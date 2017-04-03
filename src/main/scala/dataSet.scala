74a55fd909d7d957fb343562943707e6a5bca1ac
malancas <malancaster0@gmail.com>
2017-04-02 23:09:38 -0400

    Developed normalize function. Added higher order looping go function and functions that are used as its parameters

--- src/main/scala/dataSet.scala
+++ src/main/scala/dataSet.scala
@@ -1,20 +1,39 @@
-import math
+import scala.math
 
 sealed trait DataSet {
   val data
   def addDataFromFile(filename: String): Unit
 
+  def recordExtremes(i: Int, k: Int): List[Double] = {
+    if (data(i)(k) < minAtt(k)) min(k) = data(i)(k)
+    if (data(i)(k) < maxAtt(k)) maxAtt(k) = data(i)(k)
+  }
+
+  def doMath(i: Int, k: Int): List[Double] = {
+    data(i)(k) = (data(i)(k) - minAtt(k)) / (maxAtt(k) - minAtt(k))
+  }
+
+  def go(i: Int, k: Int, f: Int, Int => List[Double]): Double = {
+    if (i < data.length) {
+      if (k < numAttributes) {
+        f(i, k)
+      }
+    }
+    go(i+1, k+1, f)
+  }
+
   def normalize(): Double = {
     val numOfAttributes = (data.length - 1)
     val maxAtt = (math.NEG_INF_DOUBLE * numOfAttributes)
     val minAtt = (math.POS_INF_DOUBLE * numOfAttributes)
+
+    go(0, 0, recordExtremes)
+    go(0, 0, doMath)
   }
 }
 
-case object Nil extends DataSet {
-  val data = List()
-}
+case object Nil extends DataSet[Nothing]
 
-case class Data extends DataSet {
-  val data = //get data
+case class Data(data: List[Double]) extends DataSet {
+  val data //get data
 }
